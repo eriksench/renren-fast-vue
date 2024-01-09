032abccc05f7c8f8c90d9c08812c9eaa36186096
zhangyuxin <yuxinzh1226@qq.com>
2024-01-09 16:53:36 +0800

    p48

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,10 +1,26 @@
 <!--  -->
 <template>
-  <el-tree
-    :data="menus"
-    :props="defaultProps"
-    @node-click="handleNodeClick"
-  ></el-tree>
+  <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick">
+    <span class="custom-tree-node" slot-scope="{ node, data }">
+      <span>{{ node.label }}</span>
+      <span>
+        <el-button
+          v-if="node.level <= 2"
+          type="text"
+          size="mini"
+          @click="() => append(data)"
+          >Append</el-button
+        >
+        <el-button
+          v-if="node.childNodes.length == 0"
+          type="text"
+          size="mini"
+          @click="() => remove(node, data)"
+          >Delete</el-button
+        >
+      </span>
+    </span>
+  </el-tree>
 </template>
 
 <script>
